d9a6de0e9d3c5efcdace6ac4b8891fbb8e7f4665
mikalowdeyon <mikarobles12@gmail.com>
2025-12-02 22:37:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+(async function checkForUpdates() {
+    const currentVersion = "1.0";
+    const versionUrl = "https://raw.githubusercontent.com/ivysone/Will-you-be-my-Valentine-/main/version.json"; 
+
+    try {
+        const response = await fetch(versionUrl);
+        if (!response.ok) {
+            console.warn("Could not fetch version information.");
+            return;
+        }
+        const data = await response.json();
+        const latestVersion = data.version;
+        const updateMessage = data.updateMessage;
+
+        if (currentVersion !== latestVersion) {
+            alert(updateMessage);
+        } else {
+            console.log("You are using the latest version.");
+        }
+    } catch (error) {
+        console.error("Error checking for updates:", error);
+    }
+})();
+
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "wehhhh",
+    "Just think about it!",
+    "Hindi naman eh :<<...",
+    "Wala na sad na ako...",
+    "Look oh I'm sad na...",
+    "Ok fine, I will stop asking...",
+    "hmp!"
+];
+
+
+let jokeIndex = 0;
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
